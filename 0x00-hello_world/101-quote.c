75c6f85a9c7381813c703fa4458d200543d79ec5
adebowale <adebowalegbemileke@gmail.com>
2023-02-16 11:12:18 -0800

    seven

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-#include <unistd.h>
-
-/**
- * main - Entry point
- * Return: Always 1
- */
-
-int main(void)
-{
-	write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return(1);
-
-}
+#include <unistd.h> 
+ #define STDERR 2 
+ /** 
+ *main - prints exactly "and that place of art is useful" 
+ * - Dora Korgar, 2015-10-19", 
+ *followed by a new line, to the standard error. 
+ *Return: Always 1(Not Success) 
+ */ 
+ int main(void) 
+ { 
+         char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
+ write(STDERR, msg, sizeof(msg) - 1); 
+ return (1); 
+ }
