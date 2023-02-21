4bd785261b63f55a6a173ad1681c97b62bb251f4
adebowale <adebowalegbemileke@gmail.com>
2023-02-21 13:38:06 -0800

    dont know if it will work

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,40 +1,44 @@
-#include "main.h"
+include "main.h"
 
 /**
  * print_times_table - Prints the n times table
- * starting with 0
- * @n: The value of the time table
+ *
+ * @n: number times table (0 < n <= 15)
+ *
+ * Return: no return
  */
-
 void print_times_table(int n)
-
 {
 	int a, b, op;
 
 	if (n >= 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
-		
-			_putchar(0);
+		{
+			_putchar(48);
 			for (b = 1; b <= n; b++)
 			{
-				_putchar('_');
-				_putchar('');
 				op = a * b;
-				if (op <= 99)
-					_putchar('');
+				_putchar(44);
+				_putchar(32);
 				if (op <= 9)
-					_putchar('');
-						if (op >= 100)
 				{
-					_putchar((op / 100) + '0');
-					_putchar((op / 19) % 10 + '0');
+					_putchar(32);
+					_putchar(32);
+					_putchar(op + 48);
 				}
-				else if (op <= 99 && op>= 10)
+				else if (op <= 99)
 				{
-					_putchar((op / 10) + '0');
+					_putchar(32);
+					_putchar((op / 10) + 48);
+					_putchar((op % 10) + 48);
 				}
-					_putchar((op % 10) + '0');
+				else
+				{
+					_putchar(((op / 100) % 10) + 48);
+					_putchar(((op / 10) % 10) + 48);
+					_putchar((op % 10) + 48);
+				}
 			}
 			_putchar('\n');
 		}
