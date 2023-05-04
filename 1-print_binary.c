5a48f06deb2f09b8d77de9934dce373c716df7d7
GbeMi27 <adebowalegbemileke@gmail.com>
2023-05-04 14:42:26 -0700

    dddd

--- 1-print_binary.c
+++ 1-print_binary.c
@@ -1,37 +1,28 @@
 #include "main.h"
+
 /**
-* print_binary - print binary
-* @n: integer to mess with
-*/
+ * print_binary - A function that prints a binary number without % or /
+ * @n: The number to be printed
+ * Return: Nothing
+ */
 void print_binary(unsigned long int n)
 {
-unsigned long int i = 1;
+	unsigned int flag = 0, max = 32768; /* 1000 0000 0000 0000 */
 
-i <<= ((sizeof(i) * 8) - 1);
-if (n == 1)
-{
-_putchar('1');
-return;
-}
-if (n == 0)
-{
-_putchar('0');
-return;
-}
-while (i > 0)
-{
-if ((i & n) == 0)
-i = i >> 1;
-else
-break;
-}
-while (i > 0)
-{
-if ((i & n) == 0)
-_putchar('0');
-else
-_putchar('1');
-i = i >> 1;
-}
-
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (max)
+	{
+		if (flag == 1 && (n & max) == 0)
+			_putchar('0');
+		else if ((n & max) != 0)
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		max >>= 1;
+	}
 }
